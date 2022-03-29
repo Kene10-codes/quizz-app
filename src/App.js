96e66e6ad468ece8263f2627cbc0bb55d2275bbe
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-03-29 13:24:45 +0100

    fetched the questions from the api and rendered in the quiz question component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
+import QuizQuestions from './components/QuizQuestions';
 import './App.css';
 
 function App() {
-  // const [questions, setQuestions] = use
-  //  const ApiUrl = "https://opentdb.com/api.php?amount=10&category=12&difficulty=medium"
+  const [viewQuestions, setViewQuestions] = useState(true)
+
+  function toggleViewQuestions() {
+    setViewQuestions(prev => !prev)
+  }
   return (
     <main className='quiz--container'>
-        <h1 className='quiz--title'>Quizzical</h1>
-        <p className='quiz--text'>Test your knowledge by clicking the button below to answer some questions.</p>
-        <button className='quiz--start'>Start quiz</button>
+       {
+         viewQuestions  ?
+          <div className='quiz--header'>
+            <h1 className='quiz--title'>Quizzical</h1>
+            <p className='quiz--text'>Test your knowledge by clicking the button below to answer some questions.</p>
+            <button onClick={toggleViewQuestions} className='quiz--start'>Start quiz</button>
+          </div> 
+         :
+         <QuizQuestions />
+       }
     </main>
   );
 }
--- src/components/QuizQuestions.js
+++ src/components/QuizQuestions.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
+
 export default function QuizQuestions() {
+  const [getQuestions, setAllQuestions] = useState([])
+  // const [getQuestion, setQuestion] = useState({
+  //   correct_answer: "",
+  //   question: "What is your name",
+  // })
+  const [errors, setError] = useState("")
+  
+
+  const ApiUrl = "https://opentdb.com/api.php?amount=10&category=12&difficulty=medium"
+  
+  useEffect(() => {
+    fetch(ApiUrl)
+       .then(response => response.json())
+       .then(data => setAllQuestions(data.results))
+       .catch(error => error ? setError(errors) : error)
+  }, []) 
+
+
+   const question = getQuestions.map(obj => <p>{obj.question}</p>)
+
     return (
         <>
-          Questions
+          {question}
         </>
     )
 }
