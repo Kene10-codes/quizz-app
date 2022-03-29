23d180c8180378872c7adf283efea6c7fc82d1e8
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-03-29 23:45:05 +0100

    call all the possibe responses from the API and implemted UI

--- src/components/QuizQuestions.js
+++ src/components/QuizQuestions.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 export default function QuizQuestions() {
   const [getQuestions, setAllQuestions] = useState([])
+  const [respondAnswers, setRespondAnswers] = useState([])
   // const [getQuestion, setQuestion] = useState({
   //   correct_answer: "",
   //   question: "What is your name",
@@ -14,16 +15,32 @@ export default function QuizQuestions() {
   useEffect(() => {
     fetch(ApiUrl)
        .then(response => response.json())
-       .then(data => setAllQuestions(data.results))
+       .then(data => {
+        setAllQuestions(data.results)
+        setRespondAnswers(data.results)
+       })
        .catch(error => error ? setError(errors) : error)
   }, []) 
 
 
-   const question = getQuestions.map(obj => <p>{obj.question}</p>)
+   const question = getQuestions.map(obj => (
+     <div className='quiz--wrapper'>
+       <p className='quiz--question'>{obj.question}</p>
+       <div className='quiz--answers'>
+          <button className='quiz--btn'>{obj.correct_answer}</button>
+          {obj.incorrect_answers.map( el => (
+                <button className='quiz--btn'>{el}</button>
+          ))}
+       </div>
+     </div>
+   ))
+  //  const answer = respondAnswers.map(obj => <button className='quiz--btn'>{obj.correct_answer}</button>)
 
     return (
-        <>
+        <div className="question--container">
           {question}
-        </>
+          <button className='check--answerBtn'>Check Anwers</button>
+          {/* {answer} */}
+        </div>
     )
 }
